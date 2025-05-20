c74783fdec30f4a36a736f464aedec352ff6ff67
jubairmizan <jubairmizan@gmail.com>
2025-05-20 16:05:46 +0600

    changes

--- next.config.js
+++ next.config.js
@@ -5,23 +5,19 @@ const nextConfig = {
     domains: ["nabil-backend-dev.projectsample.info"],
   },
   env: {
+    // Default API URL for production (Vercel)
     NEXT_PUBLIC_API_URL: "https://nabil-backend-dev.projectsample.info",
   },
   async headers() {
     return [
       {
+        // Apply these headers to all routes
         source: "/:path*",
         headers: [
           {
             key: "Content-Security-Policy",
-            value: `
-              upgrade-insecure-requests;
-              default-src 'self';
-              script-src 'self' 'unsafe-inline' 'unsafe-eval';
-              connect-src 'self' https: http: https://nabil-backend-dev.projectsample.info http://localhost:8000 https://nabil-dev.old19.com ws://localhost:8282 ws://localhost:8182;
-              img-src 'self' data:;
-              style-src 'self' 'unsafe-inline';
-            `.replace(/\s{2,}/g, ' ').trim()
+            value:
+              "upgrade-insecure-requests; default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; connect-src 'self' https: http: https://nabil-backend-dev.projectsample.info http://localhost:8000; img-src 'self' data:; style-src 'self' 'unsafe-inline';",
           },
         ],
       },
